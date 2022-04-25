1804d12f509afe39bd8b39a6565fde6bf5d1998b
Alex Dorado <a.doradonavarro@gmail.com>
2022-04-25 16:27:57 +0200

    Created final grid structure and added some animations

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # Palette:
 https://colorhunt.co/palette/222831393e4600adb5eeeeee
 https://brittanychiang.com/#about
+https://www.noupe.com/design/web-developer-portfolio-examples.html
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
